e1fca2f444c8431d96507647c54fd73424c63dc9
Ezzeorazi <ezequiel.orazi90@gmail.com>
2023-11-15 17:43:00 -0300

    Change: ajustes de interfaz

--- app/categorias/javascript/page.js
+++ app/categorias/javascript/page.js
@@ -3,6 +3,7 @@
 import Editor from "@monaco-editor/react"
 import { useRef, useState } from "react"
 import SendButton from "../../_components/sendButton"
+import Footer from "@/app/Footer"
 
 const JavascriptPrueba = () => {
 
@@ -22,40 +23,65 @@ const JavascriptPrueba = () => {
 
   return (
     <>
-      <div>
-        <div>JavascriptPrueba</div>
-        <h5>Inicializa una constante</h5>
-        <div className="boxJsEditor">
-          <Editor
-            id="inputTexto"
-            type="text"
-            name="script"
-            theme="vs-dark"
-            defaultLanguage="javascript"
-            height="30vh"
-            options={{
-              fontSize: "16px",
-            }}
-            onChange={(value) => setEjercicio(value)}
-            onMount={handleEditorDidMount}
-          />
-          <div className="text-white">
-            <h6>Consola</h6>
-            <p>El resultado de tu prueba irá aquí</p>
-          </div>
+  <div className="container">
+    <div className="text-center my-5">
+      <button className="btn btn-dark rounded-0 w-50">Curso</button>
+      <button className="btn btn-secondary rounded-0 w-50">Nivel(Básico)</button>
+    </div>
+
+    <div className="row">
+      <div className="col-md-6" style={{background: '#739e2d'}}>
+        <h3 className="m-2">Ejercicio 1: Hola Mundo</h3>
+        <p className="m-2"><strong>Objetivo:</strong> Escribe y ejecuta tu primer código en JavaScript para mostrar el mensaje "Hola Mundo"</p>
+        <div className="m-2"><strong>Esperado:</strong>
+        <ul>
+          <li>Deberías ver en la consola el mensaje "Hola Mundo".</li>
+          <li>Si ves el mensaje, ¡felicidades! Has completado tu primer programa en JavaScript.</li>
+        </ul>
+        <div className="container bg-white">
+        <p style={{height: '100px'}}>*Tip por si hay un error en tu respuesta</p>
         </div>
-        <div className="scriptButtons">
-          <SendButton
-            className={`queryButton`}
-            type="submit"
-            onClick={() => setToExecute(true)}
-            buttonText={"Ejecuta tu script"}
-          >
-            Ejecutar script
-          </SendButton>
         </div>
       </div>
-    </>
+      
+      <div className="col-md-6">
+        <Editor
+          id="inputTexto"
+          type="text"
+          name="script"
+          theme="vs-dark"
+          defaultLanguage="javascript"
+          height="60vh"
+          options={{
+            fontSize: "16px",
+          }}
+          onChange={(value) => setEjercicio(value)}
+          onMount={handleEditorDidMount}
+        />
+        <div className="text-white bg-dark">
+          <h6>Consola</h6>
+          <p>El resultado de tu prueba irá aquí</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="text-end mt-4">
+      <SendButton
+        className="queryButton"
+        type="submit"
+        onClick={() => setToExecute(true)}
+        buttonText="Ejecuta tu script"
+      >
+        Ejecutar script
+      </SendButton>
+    </div>
+    <div className="d-flex justify-content-between my-5">
+      <button className="btn btn-secondary">Anterior</button>
+      <button className="btn btn-secondary">Siguiente</button>
+    </div>
+  </div>
+</>
+
   )
 }
 
