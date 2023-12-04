3392395b30be71f07a6cf7fe76a02996a1429c3c
javiergp23 <javgon23@hotmail.com>
2023-12-03 22:05:53 -0300

    api learningPath

--- app/_components/learningPath/learningPath.js
+++ app/_components/learningPath/learningPath.js
@@ -3,29 +3,31 @@ import { useState } from "react";
 import { Roboto, Inter } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Button } from "bootstrap";
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["300", "700"],
 });
 
-const Categories = ({ categories }) => {
 
-  console.log(categories)
+  
+  const LearningPath = ({ learningPath }) => {
+    
+    // console.log(getData)
 
-  const [active, setActive] = useState([true, ...Array(categories.length - 1).fill(false)]);
-
-  const handleClick = (index) => {
-    const newActive = Array(categories.length).fill(false); // Create a new array with all categories set to false
-
-    // Set the clicked category to true
-    newActive[index] = true;
-
-    setActive(newActive);
-  }
-  return (  
-    <>
-      <div className="row">
+    const [active, setActive] = useState([true, ...Array(learningPath.length - 1).fill(false)]);
+      
+      const handleClick = (index) => {
+        const newActive = Array(learningPath.length).fill(false); // Create a new array with all categories set to false
+    
+        // Set the clicked category to true
+        newActive[index] = true;
+    
+        setActive(newActive);
+      }
+    return  (
+        <>
+      {/* <div className="row">
         <div className="collapse-navbar col-12 d-md-none">
           <nav className="navbar navbar-expand-lg navbar-light ">
             <div className="container-fluid">
@@ -34,12 +36,12 @@ const Categories = ({ categories }) => {
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                  {categories.map((element, index) => (
-                    <li key={element.id} className="nav-item" >
+                  {learningPath.map((element, index) => (
+                    <li key={element.id} className="nav-item">
                       <Link className="nav-link active" style={{
                         color: ` ${active[index] ? "#739E2D" : "#000"}`,
                       }} aria-current="page" href="#" onClick={() => handleClick(index)}>
-                        {element.name}
+                        {element.names}
                       </Link>
                     </li>
                   ))}
@@ -48,29 +50,21 @@ const Categories = ({ categories }) => {
             </div>
           </nav>
         </div>
-      </div>
-
+      </div> */}
 
       <div className="dashboard">
         <div className={`${roboto.className} sidebar`}>
-        <ul className="navbar-nav"></ul>
           <ul className="navbar-nav">
-            {categories.map((category, index) => (
-              <li key={category.id}>
+            {learningPath.map((element, index) => (
+              <li key={element.id}>
                 <Link style={{
                   color: ` ${active[index] ? "#739E2D" : "#000"}`,
                 }}
                   href="#" onClick={() => handleClick(index)}>
-                  {category.name}
+                  {element.name}
                   <small>
-                    {" "}
-                    (
-                    {
-                      category.exercises.filter(
-                        (exercise) => exercise.completed == true
-                      ).length
-                    }{" "}
-                    / {category.exercises.length})
+                    {""}
+                
                   </small>
                 </Link>
               </li>
@@ -80,21 +74,24 @@ const Categories = ({ categories }) => {
 
 
         <div className={`${roboto.className} content`}>
-          {categories.map((element, index) => (
+            <button><Link  href={'/learningPath'} >Javascript</Link></button>
+            <button><Link href={'/categorias'}>SQL</Link></button>
+          {learningPath.map((element, index) => (
             <div key={element.id}>
               <h3 key={index} className={`${active[index] ? "submenu active" : "d-none"} mb-4`}>
-                {element.name}
+                {element.company.name}
               </h3>
+
               {/* <p className={`${active[index] ? "submenu active" : "d-none"} mb-4`}>
                 Texto por definir para cada categoria
               </p> */}
-              <ul key={categories.id} className={`${active[index] ? "submenu active" : "d-none"}  `}>
-                {element.exercises.map((sub) => (
+              <ul key={learningPath.id} className={`${active[index] ? "submenu active" : "d-none"}  `}>
+                {learningPath.map((sub) => (
                     <li key={sub.id}
                     >
                       <Link
                         className={`${"link-menu"}`}
-                        href={`/categorias/${element.id}/ejercicios/${sub.id}`}
+                        href={``}
                       >
                         <button className="span-categories ">
                           {sub.completed ? (
@@ -102,7 +99,7 @@ const Categories = ({ categories }) => {
                           ) : (
                             <Image src="/images/elipse.png" height={17} width={17} alt="Academia Desafío Latam" title="Academia Desafío Latam" />
                           )}
-                          {sub.title}
+                          {sub.company.name}
                         </button>
                       </Link>
                     </li>
@@ -113,7 +110,9 @@ const Categories = ({ categories }) => {
         </div>
       </div>
     </>
-  );
-};
+    )
+  }
 
-export default Categories;
+ 
+
+export default LearningPath;
--- app/learningPath/page.js
+++ app/learningPath/page.js
@@ -3,39 +3,24 @@ import { useState, useEffect } from "react";
 import { Roboto, Inter } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import LearningPath from "../_components/learningPath/learningPath";
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["300", "700"],
-});
-
+ 
   async function getData () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
+    
     console.log(data)
+    return data;
   }
   
-  function LearningPath() {
-    getData()
+ export default async function Page() {
+   const users = await getData();
+
     return (
-        <h1>Learning Path</h1>
-      
-    )
+      <>
+        <LearningPath learningPath={users}/>
+      </>
+        )
   }
 
-  // console.log(categories)
-
-
-  // const [active, setActive] = useState([true, ...Array(categories.length - 1).fill(false)]);
-
-  // const handleClick = (index) => {
-  //   const newActive = Array(categories.length).fill(false); // Create a new array with all categories set to false
-
-  //   // Set the clicked category to true
-  //   newActive[index] = true;
-
-  //   setActive(newActive);
-  // }
-  
-
-export default LearningPath;
