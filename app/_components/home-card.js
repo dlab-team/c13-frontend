4590435df93599676d450151140dc0a613ad5df4
BotDeIA <arroyojavier.sanandres@gmail.com>
2023-12-05 15:22:35 -0300

    diseño home movil first, imagenes de carta agregadas

--- app/_components/home-card.js
+++ app/_components/home-card.js
@@ -1,56 +1,22 @@
 "use client"
-import { useState } from "react";
+//import { useState } from "react";
 import "./home-card.css"
-import HomeModal from "./home-modal"
-import { Roboto } from "next/font/google";
+//import { Roboto } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
-const roboto = Roboto({
-  weight: ['300', '700'],
-  subsets: ['latin'],
-});
+// const roboto = Roboto({
+//   weight: ['300', '700'],
+//   subsets: ['latin'],
+// });
 
 
-export default function HomeCard({ last = false, title, content }) {
-  const [openDialog, setOpenDialog] = useState(false)
-
-  const showDialog = () => {
-    setOpenDialog(true)
-  }
-
+export default function HomeCard({lenguage = "", url_img }) {
   return (
     <>
-      <div className={`why_card ${last ? "and_more" : ""}`} >
-        <section className="img_card">
-          {content
-            ? <Image src={content.card_img} width={500} height={500} alt="lenguaje_img" />
-            : ""}
-        </section>
-        <section className="content_card">
-          <div className="title_card">
-            <h3
-              className="title_text"
-            >Curso {title}</h3>
-          </div>
-          <p className="description_card">
-            {content.card_description}
-          </p>
-          <div className="button_container_card">
-            <button
-              className={`${roboto.className} buttons`}
-              onClick={showDialog}
-            >Por que {title}?</button>
-          </div>
-        </section>
-        {!last ?
-          <HomeModal
-            title={title}
-            content={content.content_modal}
-            learningPathContent={content.learning_path}
-            show={openDialog}
-            setShow={setOpenDialog}
-          ></HomeModal> : ""}
-      </div>
+      <Link href={`/que-aprenderas/${lenguage.replace(" ", "-")}`}>
+        <Image src={url_img} width={361} height={141} alt="lenguage cart" ></Image>
+      </Link>
     </>
   )
 }
