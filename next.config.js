7baf047bd7149cedfa95082af10d3c2a24af3b36
Leonardo Jose <ljcl79@gmail.com>
2023-12-07 20:21:35 -0300

    Generar variable de ambiente HOST

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   env: {
     API_BASE_URL: "https://tutorial-interactivo-sql.onrender.com/api/v1",
     JUDGE0_URL: process.env.JUDGE0_URL,
+    HOST: process.env.HOST,
   },
   webpack: (config, { isServer, webpack }) => {
     console.log(`Webpack version: ${webpack.version}`);
