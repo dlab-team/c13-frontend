255d53c81c2472bd59e4d0f3b423c57a31520af4
Leonardo Jose <ljcl79@gmail.com>
2023-12-07 22:08:41 -0300

    Convertir LearningPath en SSR

--- app/learningPath/page.js
+++ app/learningPath/page.js
@@ -1,9 +1,15 @@
-"use client";
+//"use client";
+import fsPromises from "fs/promises";
+import path from "path";
 import LearningPath from "../_components/learningPath/learningPath";
 
 async function getData() {
-  const res = await fetch(`${process.env.HOST}/data.json`);
-  const data = await res.json();
+  // Get the path of the json file
+  const filePath = path.join(process.cwd(), "app/learningPath/data.json");
+  // Read the json file
+  const jsonData = await fsPromises.readFile(filePath);
+  // Parse data as json
+  const data = JSON.parse(jsonData);
 
   console.log(data);
   return data;
